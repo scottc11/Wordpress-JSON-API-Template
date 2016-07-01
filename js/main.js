579cdaf71f57217ee1ce8a57f36ae7ad34932e34
Scott Campbell <scottcampbell@Scotts-MacBook-Pro.local>
2016-07-01 16:14:07 -0400

    built a function which parses out the first <img> tag in a post and returns the URL as a string

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 
 var jsonData;
+var postData;
+var postImageURL;
 
 // when page is fully loaded, get wordpress posts
 $(document).ready(function() {
@@ -21,16 +23,28 @@ $(document).ready(function() {
       }
   });
 
-  jsonData = $.getValues("http://frommusictocode.com/wp-json/wp/v2/posts");
-  console.log(jsonData[0]);
-  console.log(typeof jsonData[0].date);
+  jsonData = $.getValues("http://frommusictocode.com/wp-json/wp/v2/posts/");  // get all posts
+
+  postData = jsonData[0]; // return the most recent post
+  console.log(postData);
 
   // assign variable to jquery request so you only have to do it once
   var $postContainer = $('.post-container');
 
-  // set date of post in the p element
-  $postContainer.children('.post-date').text(jsonData[0].date);
-  // set the content of the post inside the p element
-  $postContainer.children('.post-content').text(jsonData[0].content.rendered);
+  $postContainer.children('.post-date').text(postData.date);  // set date of post in the p element
+
+  $postContainer.children('.post-content').text(postData.content.rendered);  // set the content of the post inside the p element
+
+  postImageURL = getImageURL(postData.content.rendered);  // get the first image within the post
 
 });
+
+function getImageURL(renderedHTML) {
+  var imageURL_1 = renderedHTML.split("<img"); // find the first "<img>" tag
+  var imageURL_2  = imageURL_1[1].split('src="');  // find the "scr" of the returned <img> tag
+  var imageURL_3  = imageURL_2[1].split(' ');  // split the remaining html content after the 'src' (image url)
+  var imageURL = imageURL_3[0].substring(0, imageURL_3[0].lastIndexOf('"'));  // return a string between index [0] of image url and the last quotation on the end of the url
+  console.log(imageURL);
+  console.log(typeof imageURL);
+  return imageURL;
+}
